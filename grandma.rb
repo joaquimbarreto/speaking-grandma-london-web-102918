375c40fed7964648e02e2e3f88f91e8b1400603f
Joaquim Barreto <joaquim@barreto.co>
2018-10-24 11:19:07 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 
 
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA"
+  if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == "WHAT DID YOU EAT TODAY?"
     puts "NO, NOT SINCE 1938"
